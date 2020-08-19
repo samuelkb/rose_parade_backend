2b6f1c54b764d9d7a631bdc35a0ddefffe9fcdfe
Samuelkb <samuelk.b.hc@gmail.com>
2020-08-18 23:41:54 -0500

    Participant model generated

--- participants/models.py
+++ participants/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Participant(models.Model):
+    type_choices = [
+        ('BAND', 'Band'),
+        ('CARRIAGE', 'Carriage'),
+    ]
+    name = models.CharField(max_length=50, blank=False, default='')
+    manager_email_address = models.CharField(max_length=50, blank=False, default='')
+    type_of_participant = models.CharField(max_length=8, choices=type_choices, default='BAND')
+    foundation_date = models.DateTimeField()
+    registered_date = models.DateTimeField(default=timezone.now())
+    order = models.IntegerField(default=1)
